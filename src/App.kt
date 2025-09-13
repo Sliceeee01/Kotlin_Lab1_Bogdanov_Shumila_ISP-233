d560d105f1997e91da0c1cb0dd4ef1b32b420a10
Sigma <SiGmA228@gmail.com>
2025-09-13 13:13:36 +0300

    Initial commit

---
+++ src/App.kt
@@ -0,0 +1,70 @@
+const val PI = 3.1415
+fun main(){
+//    println("Hello, world!")
+//    print("Hello\n")
+//    println("Kotlin")
+//    println("hfyfiuekmyhfkiykigfigfeyjlkehfkgkggkgkgfkggefkgfkjgkfgkfgkgkfgjfgejgtfugfkgfuetgufgjfgkfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgggggggggggggggg")
+
+
+//    val city: String = "Volgograd"
+//    println(city)
+//    var number : Int = 2
+//    number = 3
+
+//    Задание 1
+//    val name = "Ivan"
+//    var age = 18
+//    val ageborn = 2007
+//    age += 10
+//    println("Ваше имя - $name, Ваш возраст - $age, Ваш год рождение = $ageborn")
+
+//    Задание 2
+//    val int = 2
+//    var Double = 40
+//    val Boolean = 20
+//    val Char = 'A'
+//    val string = "ettmeetf"
+//    val any = 25
+
+
+//    ДЗ
+//    Задание 1
+//    val game = "Dota2"
+//    val number = 2
+//    val PI = 3.1415
+//    val bykva = 'A'
+//    println("$game , $number , $PI , $bykva ")
+
+//    Задание 2 - щшибки в коде
+//    println()
+//        string = "dhdhdh"
+//        val number = 3
+
+//   Задание 3
+//    var Double  = 3.14
+//    var Int = 5
+//    val ploashad = Double * Int * Int
+//    println("площадь круга равна - $ploashad")
+
+
+//    Задание 4
+//    print("Введите ваше имя и год рождения: ")
+//    val name = readln()
+//    val  num = readln().toInt()
+//    val  thisyear = ("${2025 - num}")
+//
+//    println("$name, $num вам: $thisyear - лет")
+
+    //задание 5
+    print("Введите первое и второе число число: ")
+
+    val num = readln().toInt()
+    val num1 = readln().toInt()
+    val sum = num + num1
+    val sum1 = num - num1
+    val sum2 = num * num1
+    println("$num + $num1 = $sum")
+    println("$num - $num1 = $sum1")
+    println("$num * $num1 = $sum2")
+
+}
