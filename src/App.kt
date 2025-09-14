ce34c489f3abbb7e5727ddc39ba49e2d1c65a57a
Sliceeee01 <ivan_hih@icloud.com>
2025-09-14 16:17:24 +0300

    Update App.kt

--- src/App.kt
+++ src/App.kt
@@ -11,6 +11,8 @@ fun main(){
 //    var number : Int = 2
 //    number = 3
 
+    //Лаба 1 Задания самой лабы
+    
 //    Задание 1
 //    val name = "Ivan"
 //    var age = 18
@@ -25,9 +27,44 @@ fun main(){
 //    val Char = 'A'
 //    val string = "ettmeetf"
 //    val any = 25
+ 
+    //Задание 3
+//    val number: Double = 3.5
+//    val number1: Int = 7
+//    val integernumber: Int = number.toInt()
+//    val sum = number + number1
+//    val sum1 = number / number1
+//    println("$sum, $sum1")
 
+   //Задание 4
+//    print("Введите ваше имя и возраст: ")
+//    val name = readln()
+//    val age = readln().toInt()
+//    val sum = age + 5
+//    println("$name, через 5 лет вам будет $sum лет" )
 
-//    ДЗ
+    //Задание 5
+//    print("Введите два числа: ")
+//    val a = readln().toInt()
+//    val b = readln().toInt()
+//    val sum = a - b
+//    val sum1 = a % b
+//    println("Сумма: ${a + b}")
+//    println("Произведение: ${a * b}")
+//    println("Частное: ${a / b}")
+//    println("Разность: $sum")
+//    println("Остаток: $sum1")
+
+//    Задание 6
+//    print("Введите число: ")
+//    val totalSeconds = readln().toInt()
+//    val hours = totalSeconds / 3600
+//    val minutes = totalSeconds % 3600 / 60
+//    val seconds = totalSeconds % 3600 % 60
+//    println("$hours ч $minutes мин $seconds сек")
+
+//    Домашнее задание
+    
 //    Задание 1
 //    val game = "Dota2"
 //    val number = 2
@@ -56,15 +93,56 @@ fun main(){
 //    println("$name, $num вам: $thisyear - лет")
 
     //задание 5
-    print("Введите первое и второе число число: ")
+//    print("Введите первое и второе число число: ")
 
-    val num = readln().toInt()
-    val num1 = readln().toInt()
-    val sum = num + num1
-    val sum1 = num - num1
-    val sum2 = num * num1
-    println("$num + $num1 = $sum")
-    println("$num - $num1 = $sum1")
-    println("$num * $num1 = $sum2")
+//    val num = readln().toInt()
+//    val num1 = readln().toInt()
+//    val sum = num + num1
+//    val sum1 = num - num1
+//    val sum2 = num * num1
+//    println("$num + $num1 = $sum")
+//    println("$num - $num1 = $sum1")
+//    println("$num * $num1 = $sum2")
 
+     //Задание 6
+//        print("Введите трехрехзначное число: ")
+//        val number = readln().toInt()
+//        val num1 = number / 100
+//        val num2 = (number % 100) / 10
+//        val num3 = number % 10
+//        val sum = num1 + num2 + num3
+//        println("$number -> $num1+$num2+$num3 = $sum")
+
+   //Задание 7
+//    print("Введите строку разделитель: ")
+//    val number = readln()
+//    val num1 = readln().toInt()
+//    val num2 = readln().toInt()
+//    val num3 = readln().toInt()
+//    println("$num1$number$num2$number$num3")
+
+    //Задание 8
+//    print("Введите число: ")
+//    val number = readln().toInt()
+//    val num1 = number
+//    val num2 = number + 1
+//    val num3 = number + 2
+//    println("$num1")
+//    println("$num2")
+//    println("$num3")
+
+    //Задание 9
+//    print("Введите трехзначное число: ")
+//    val number = readln().toInt()
+//    val num1 = readln().toInt()
+//    val num2 = readln().toInt()
+//    val num3 = readln().toInt()
+//    println("$num3$num2$num1")
+
+    //Задание 10
+//    print("Введите натуральео число: ")
+//    val number = readln().toInt()
+//    val num3 = (number / 100) % 10
+//    println("$num3")
+    
 }
